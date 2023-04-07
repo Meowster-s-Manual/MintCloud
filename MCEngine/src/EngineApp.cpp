7cf5361a43c9b07a0fcab9bba62622de5eca2205
andrew-suh <andrew.suh.94@gmail.com>
2023-04-06 19:06:24 -0700

    Encapsulated Keyboard events into a class.

--- MCEngine/src/EngineApp.cpp
+++ MCEngine/src/EngineApp.cpp
@@ -10,11 +10,10 @@ using namespace MCEngine;
 #include "EventSystem/EventBus.h"
 #include "EventSystem/Event.h"
 
-class EventA : public Event
+class ENGINE_API InputEvent : public Event
 {
 public:
-    EventA(EventBus* eventBus) : Event(eventBus) {}
-
+    InputEvent(EventBus* eventBus) : Event(eventBus) {}
     void update()
     {
         Message greeting("Bye!");
@@ -26,6 +25,23 @@ private:
     {
         std::cout << "A received: " << message.getEvent() << std::endl;
     }
+};
+
+class ENGINE_API KeyboardEvent : public InputEvent
+{
+public:
+    KeyboardEvent(EventBus* eventBus) : InputEvent(eventBus) {}
+    static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+    {
+        if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+private:
+};
+
+class ENGINE_API MouseEvent : public InputEvent 
+{
+    //TODO: Discuss if we want 2 filters here or seperate into two classes
 };
 
 // This class will send a message to ComponentA.
@@ -58,12 +74,6 @@ EngineApp::~EngineApp()
 
 }
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
-
 
 int EngineApp::start()
 {
@@ -73,12 +83,13 @@ int EngineApp::start()
     window->init("WHAT ARE YOU DOING ANDREW");
 
     EventBus eventBus;
-    EventA compA(&eventBus);
+    KeyboardEvent keebs(&eventBus);
     EventB compB(&eventBus);
+
 
     // This is supposed to act like a game loop.
     for (int ctr = 0; ctr < 5; ctr++) {
-        compA.update();
+        keebs.update();
         compB.update();
         eventBus.notify();
 
@@ -87,7 +98,7 @@ int EngineApp::start()
 
 	while (!window->windowShouldClose()) {
         window->pollEvents();
-        glfwSetKeyCallback(window->window, key_callback);
+        glfwSetKeyCallback(window->window, keebs.key_callback);
 	}
 
 	return 0;
